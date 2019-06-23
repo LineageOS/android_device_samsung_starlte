ce13936115cdaa9d2d6eeac407fd90a489fbd28d
synt4x93 <synt4x.93@gmail.com>
2019-06-23 01:33:30 +0000

    starlte: Add fingerprint override
    Change-Id: Ia32b23c4f71f13be10811c3c95e753abca087990

--- lineage_starlte.mk
+++ lineage_starlte.mk
@@ -7,3 +7,8 @@ $(call inherit-product, device/samsung/starlte/full_starlte.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_starlte
+
+BUILD_FINGERPRINT := samsung/starltexx/starlte:9/PPR1.180610.011/G960FXXU4CSE3:user/release-keys
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+        PRIVATE_BUILD_DESC="starltexx-user 9 PPR1.180610.011 G960FXXU4CSE3 release-keys"
