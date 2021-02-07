ae08d151aa6e5551cd2a51062b0ac15f8282dab9
Tim Zimmermann <tim@linux4.de>
2021-02-07 16:30:10 +0100

    starlte: patchelf libsensorlistener to load libshim_sensorndkbridge
    Change-Id: I2d9f2aa60d64cd6cabfd904c53b536606f18438e

--- extract-files.sh
+++ extract-files.sh
@@ -53,6 +53,15 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib/libsensorlistener.so)
+            "${PATCHELF}" --add-needed libshim_sensorndkbridge.so "${2}"
+            ;;
+    esac
+}
+
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
