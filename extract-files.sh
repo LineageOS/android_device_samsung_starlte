ab7054eef427321e775355334eca8ead1634b897
synt4x93 <synt4x.93@gmail.com>
2021-10-28 04:31:54 +0100

    starlte: proprietary: commonise blobs
    Co-authored-by: josip-k <d3hum4niz3d@gmail.com>
    Change-Id: Ib5bdda483893cff18835830a6d945231c3afff8f

--- extract-files.sh
+++ extract-files.sh
@@ -6,69 +6,27 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-set -e
-
-DEVICE=starlte
-VENDOR=samsung
-
-# Load extract_utils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
-
-ANDROID_ROOT="${MY_DIR}/../../.."
-
-HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
-if [ ! -f "${HELPER}" ]; then
-    echo "Unable to find helper script at ${HELPER}"
-    exit 1
-fi
-source "${HELPER}"
-
-# Default to sanitizing the vendor folder before extraction
-CLEAN_VENDOR=true
-
-KANG=
-SECTION=
-
-while [ "${#}" -gt 0 ]; do
-    case "${1}" in
-        -n | --no-cleanup )
-                CLEAN_VENDOR=false
-                ;;
-        -k | --kang )
-                KANG="--kang"
-                ;;
-        -s | --section )
-                SECTION="${2}"; shift
-                CLEAN_VENDOR=false
-                ;;
-        * )
-                SRC="${1}"
-                ;;
-    esac
-    shift
-done
-
-if [ -z "${SRC}" ]; then
-    SRC="adb"
-fi
-
 function blob_fixup() {
     case "${1}" in
-        vendor/lib*/libsec-ril.so|vendor/lib*/libsec-ril-dsds.so)
-            sed -i "s/libhidltransport.so/libcutils-v29.so\x00\x00\x00/" "${2}"
-            ;;
-        vendor/lib*/libsensorlistener.so)
-            "${PATCHELF}" --add-needed libshim_sensorndkbridge.so "${2}"
+	vendor/lib*/libwrappergps.so)
+	    "${PATCHELF}" --replace-needed libvndsecril-client.so libsecril-client.so "${2}"
+	    ;;
+        vendor/bin/hw/android.hardware.neuralnetworks@1.2-service-armnn)
+            "${PATCHELF}" --add-needed libdemangle.so "${2}"
             ;;
     esac
 }
 
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
 
-# Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
+set -e
 
-extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
-extract "${MY_DIR}/proprietary-files-vendor.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+export DEVICE=starlte
+export DEVICE_COMMON=exynos9810-common
+export VENDOR=samsung
 
-"${MY_DIR}/setup-makefiles.sh"
+"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -8,30 +8,8 @@
 
 set -e
 
-DEVICE=starlte
-VENDOR=samsung
+export DEVICE=starlte
+export DEVICE_COMMON=exynos9810-common
+export VENDOR=samsung
 
-# Load extract_utils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
-
-ANDROID_ROOT="${MY_DIR}/../../.."
-
-HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
-if [ ! -f "${HELPER}" ]; then
-    echo "Unable to find helper script at ${HELPER}"
-    exit 1
-fi
-source "${HELPER}"
-
-# Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
-
-# Warning headers and guards
-write_headers
-
-write_makefiles "${MY_DIR}/proprietary-files.txt" true
-write_makefiles "${MY_DIR}/proprietary-files-vendor.txt" true
-
-# Finish
-write_footers
+"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
