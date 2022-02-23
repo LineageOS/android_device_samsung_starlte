05c301857c6fa989534c69236424a07e3acad70d
baddar90 <mnbaddar1990@gmail.com>
2022-02-23 11:00:56 -0800

    starlte: update props from G960FXXUHFVK1
    Change-Id: Ib45c11f3b1bd60c40660493744a73be0acad2966

--- lineage_starlte.mk
+++ lineage_starlte.mk
@@ -39,3 +39,13 @@ PRODUCT_MODEL := SM-G960F
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+BUILD_FINGERPRINT := "samsung/starltexx/starlte:10/QP1A.190711.020/G960FXXUHFVK1:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=starltexx \
+    PRIVATE_BUILD_DESC="starltexx-user 10 QP1A.190711.020 G960FXXUHFVK1 release-keys"
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.PDA=G960FXXUHFVK1 \
+    ro.build.fingerprint=samsung/starltexx/starlte:10/QP1A.190711.020/G960FXXUHFVK1:user/release-keys
